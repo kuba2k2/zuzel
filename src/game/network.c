332ea89cf7dea21992a07701e54566d32dae4a52
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2025-01-08 18:54:53 +0100

    [game] Implement expiry timer

--- src/game/network.c
+++ src/game/network.c
@@ -2,6 +2,9 @@
 
 #include "game.h"
 
+static void game_check_empty(game_t *game);
+static uint32_t game_expiry_cb(uint32_t interval, game_t *game);
+
 void game_add_endpoint(game_t *game, net_endpoint_t *endpoint) {
 	net_endpoint_t *item = net_endpoint_dup(endpoint);
 	if (item == NULL)
@@ -12,6 +15,8 @@ void game_add_endpoint(game_t *game, net_endpoint_t *endpoint) {
 	if (endpoint->type <= NET_ENDPOINT_TLS && !game->is_client)
 		// clients don't send endpoint connection updates
 		game_request_send_game_data(game);
+	// cancel the expiry timer
+	game_check_empty(game);
 }
 
 void game_del_endpoint(game_t *game, net_endpoint_t *endpoint) {
@@ -24,17 +29,38 @@ void game_del_endpoint(game_t *game, net_endpoint_t *endpoint) {
 	net_endpoint_free(endpoint);
 	free(endpoint);
 	// stop the game if there are no more endpoints
-	int endpoints;
+	game_check_empty(game);
+}
+
+static void game_check_empty(game_t *game) {
+	int endpoints = 0;
 	SDL_WITH_MUTEX(game->mutex) {
 		net_endpoint_t *item;
 		DL_COUNT(game->endpoints, item, endpoints);
+		if (game->expiry_timer != 0) {
+			LT_I("Game: clearing expiry timer");
+			SDL_RemoveTimer(game->expiry_timer);
+			game->expiry_timer = 0;
+		}
+		if (endpoints == 1) {
+			LT_I("Game: empty, setting expiry timer");
+			game->expiry_timer = SDL_AddTimer(60000, (SDL_TimerCallback)game_expiry_cb, game);
+		}
 	}
-	if (endpoints == 1)
+}
+
+static uint32_t game_expiry_cb(uint32_t interval, game_t *game) {
+	LT_I("Game: expired '%s' (key: %s)", game->name, game->key);
+	SDL_WITH_MUTEX(game->mutex) {
 		game->stop = true;
+	}
+	// wake up the game thread
+	game_request_send_game_data(game);
+	return 0;
 }
 
 void game_send_packet_pipe(game_t *game, pkt_t *pkt) {
-	net_endpoint_t *pipe;
+	net_endpoint_t *pipe = NULL;
 	SDL_WITH_MUTEX(game->mutex) {
 		DL_FOREACH(game->endpoints, pipe) {
 			if (pipe->type == NET_ENDPOINT_PIPE)
